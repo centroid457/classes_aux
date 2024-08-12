51dadaa60e315a8507a97cb2c1b0c6339a1db839
Andrei Starichenko <centroid@mail.ru>
2024-08-12 17:20:23 +0300

    [MiddleGroup] ref middle_group__check_equal

--- classes_aux/middle_group.py
+++ classes_aux/middle_group.py
@@ -1,18 +1,92 @@
 from typing import *
 from object_info import *
+from funcs_aux import *
 
 
 # =====================================================================================================================
 class ClsMiddleGroup:
     """
-    if you need to separate instances into several groups.
-    main ide is manipulating with grouped methods? not just to keep name in attribute.
+    GOAL
+    ----
+    if you need to separate cls/inst into several groups by middle nested class.
+    main idea is manipulating with grouped methods, not just name in attribute.
 
     USAGE
     -----
-    !. dont use this class as final! only middle!
     1. create some middle classes which will define groups.
-    2. do nesting your final classes by Groups
+    2. apply group methods and mention it in MIDDLE_GROUP__CMP_METH so objects could be compared not just by names!
+    3. do nesting your final classes by Groups
+
+        # BEFORE --------------------------
+        class YourClsBase:
+            pass
+
+        class Cls1(YourClsBase):
+            GROUP__NAME = "Group1"
+            def meth(self):
+                pass
+
+        class Cls2(YourClsBase):
+            GROUP__NAME = "Group2"
+            def meth(self):
+                return True
+
+        class Cls3(YourClsBase):
+            GROUP__NAME = "Group2"
+            def meth(self):
+                return True
+
+        # AFTER --------------------------
+        class Group1(ClsMiddleGroup):
+            MIDDLE_GROUP__NAME = "Group1"
+            def meth(self):
+                pass
+
+        class Group2(ClsMiddleGroup):
+            MIDDLE_GROUP__NAME = "Group2"
+            def meth(self):
+                return True
+
+        class YourClsBase(ClsMiddleGroup):
+            pass
+
+        class Cls1(Group1, YourClsBase):
+            pass
+
+        class Cls2(Group2, YourClsBase):
+            pass
+
+        class Cls3(Group2, YourClsBase):
+            pass
+
+
+    TWO WAYS TO SEPARATE GROUPS
+    NOTE: BEST WAY: use only *_CMP_METH! *_NAME useful for issues when you need to know exactly the group name!
+    but you could forget using MIDDLE_GROUP__CMP_METH and exists incorrect cases.
+
+    1. NAME/str attribute as base (always checks first) - MIDDLE_GROUP__NAME
+        class Group1(ClsMiddleGroup):
+            MIDDLE_GROUP__NAME = "Group1"
+            def meth(self):
+                pass
+
+        class Group2(ClsMiddleGroup):
+            MIDDLE_GROUP__NAME = "Group2"
+            def meth(self):
+                return True
+
+    2. by other methods in addition - MIDDLE_GROUP__CMP_METH
+        class GroupBase(ClsMiddleGroup):
+            MIDDLE_GROUP__METH_CMP = "meth"
+            MIDDLE_GROUP__METH_CMP = ["meth", "meth2"]
+
+        class Group1(GroupBase):
+            def meth(self):
+                pass
+
+        class Group2(GroupBase):
+            def meth(self):
+                return True
 
     CREATED SPECIALLY FOR
     ---------------------
@@ -20,25 +94,55 @@ class ClsMiddleGroup:
     need to handle testcase classes as some groups!
     there was not enough separating process just by startup_cls and startup_inst!!! need startup_group!
     """
-
-    MIDDLE_GROUP_NAME: str | None = None  # this is not for comparing! just for short clear name instead of not useful ClsName
-
-    @staticmethod
-    def middle_group__check_exists(other: Any) -> bool:
-        return hasattr(other, "middle_group__get")
-
-    @classmethod
-    def middle_group__get(cls) -> str:
-        for cls_i in cls.__mro__[1:]:
-            if hasattr(cls_i, "middle_group__get"):
-                return cls_i.__name__
+    MIDDLE_GROUP__NAME: None | str = None           # main cmp meth
+    MIDDLE_GROUP__CMP_METH: TYPE__ARGS = None       # additional cmp methods
 
     @classmethod
     def middle_group__check_equal(cls, other: Any) -> bool | None:
-        if not ClsMiddleGroup.middle_group__check_exists(other):
+        """
+        middle groups could be cmp only by this method!
+        """
+        if not TypeChecker.check__nested__by_cls_or_inst(other, ClsMiddleGroup):
             return
 
-        return cls.middle_group__get() == other.middle_group__get()
+        # CMP not only by just one name!!! need cmp by equity special methods!
+        result = cls.MIDDLE_GROUP__NAME == other.MIDDLE_GROUP__NAME
+        if not result:
+            return False
+
+        if cls.MIDDLE_GROUP__CMP_METH or other.MIDDLE_GROUP__CMP_METH:
+            # collect ------------------
+            meths_cmp = []
+            if cls.MIDDLE_GROUP__CMP_METH:
+                meths_cmp1 = args__ensure_tuple(cls.MIDDLE_GROUP__CMP_METH)
+                if meths_cmp1:
+                    meths_cmp.extend(meths_cmp1)
+
+            if other.MIDDLE_GROUP__CMP_METH:
+                meths_cmp2 = args__ensure_tuple(other.MIDDLE_GROUP__CMP_METH)
+                if meths_cmp2:
+                    meths_cmp.extend(meths_cmp2)
+
+            # cmp ------------------
+            other = ensure_class(other)
+
+            for meth_name in meths_cmp:
+                hasattr1 = hasattr(cls, meth_name)
+                hasattr2 = hasattr(other, meth_name)
+                if not hasattr1 and not hasattr2:
+                    continue
+
+                try:
+                    getattr1 = getattr(cls, meth_name)
+                    getattr2 = getattr(other, meth_name)
+                    result &= getattr1 == getattr2
+                except:
+                    result = False
+
+                if not result:
+                    return False
+
+        return result
 
     # -----------------------------------------------------------------------------------------------------------------
     # EXAMPLE for TCS
--- tests/test__middle_group.py
+++ tests/test__middle_group.py
@@ -1,115 +1,46 @@
 from typing import *
 import pathlib
-
 import pytest
 from pytest import mark
 from pytest_aux import *
-
 from classes_aux import *
 
 
 # =====================================================================================================================
-class ClsMiddleGroup01(ClsMiddleGroup):
-    pass
-
-
-class ClsMiddleGroup02(ClsMiddleGroup):
-    pass
-
-
-class ClsMiddleGroup11(ClsMiddleGroup01):
-    pass
-
-
-# =====================================================================================================================
-# GROUPS---------------------------------------------------------------------------------------------------------------
-@pytest.mark.parametrize(
-    argnames="args, _EXPECTED",
-    argvalues=[
-        (True, False),
-        (1, False),
-        (bool, False),
-
-        (ClsMiddleGroup, True),
-        (ClsMiddleGroup(), True),
-        (ClsMiddleGroup01, True),
-        (ClsMiddleGroup01(), True),
-        (ClsMiddleGroup02, True),
-        (ClsMiddleGroup02(), True),
-        (ClsMiddleGroup11, True),
-        (ClsMiddleGroup11(), True),
-    ]
-)
-def test__check_exists(args, _EXPECTED):
-    func_link = ClsMiddleGroup.middle_group__check_exists
-    pytest_func_tester__no_kwargs(func_link, args, _EXPECTED)
-
-
-# -----------------------------------------------------------------------------------------------------------------
-@pytest.mark.parametrize(
-    argnames="args, _EXPECTED, _MARK",
-    argvalues=[
-        (True, AttributeError, None),
-        (1, AttributeError, None),
-        (bool, AttributeError, None),
-
-        (ClsMiddleGroup, "ClsMiddleGroup", mark.xfail),         # DONT TRY TO USE DIRECT TO GROUP!
-        (ClsMiddleGroup(), "ClsMiddleGroup", mark.xfail),
-        (ClsMiddleGroup01, "ClsMiddleGroup01", mark.xfail),
-        (ClsMiddleGroup01(), "ClsMiddleGroup01", mark.xfail),
-        (ClsMiddleGroup02, "ClsMiddleGroup02", mark.xfail),
-        (ClsMiddleGroup02(), "ClsMiddleGroup02", mark.xfail),
-        (ClsMiddleGroup11, "ClsMiddleGroup11", mark.xfail),
-        (ClsMiddleGroup11(), "ClsMiddleGroup11", mark.xfail),
-    ]
-)
-def test__group__name_get(args, _EXPECTED, _MARK):
-    func_link = lambda obj: obj.middle_group__get()
-    pytest_func_tester__no_kwargs(func_link, args, _EXPECTED, _MARK)
-
-
-# =====================================================================================================================
-# VICTIM --------------------------------------------------------------------------------------------------------------
 class Victim(ClsMiddleGroup):
     pass
 
 
-class Victim01(ClsMiddleGroup01):
+class Victim10(ClsMiddleGroup):
+    MIDDLE_GROUP__NAME = "name1"
     pass
 
 
-class Victim02(ClsMiddleGroup02):
+class Victim20(ClsMiddleGroup):
+    MIDDLE_GROUP__NAME = "name2"
     pass
 
 
-class Victim11(ClsMiddleGroup11):
+class VictimMeth(ClsMiddleGroup):
+    def meth_cmp(self):
+        pass
     pass
 
 
-# ---------------------------------------------------------------------------------------------------------------------
-@pytest.mark.parametrize(
-    argnames="obj, _EXPECTED",
-    argvalues=[
-        (True, AttributeError),
-        (1, AttributeError),
-        (bool, AttributeError),
-
-        (Victim, "ClsMiddleGroup"),
-        (Victim(), "ClsMiddleGroup"),
-        (Victim01, "ClsMiddleGroup01"),
-        (Victim01(), "ClsMiddleGroup01"),
-        (Victim02, "ClsMiddleGroup02"),
-        (Victim02(), "ClsMiddleGroup02"),
-        (Victim11, "ClsMiddleGroup11"),
-        (Victim11(), "ClsMiddleGroup11"),
-    ]
-)
-def test__victim__name_get(obj, _EXPECTED):
-    func_link = lambda: obj.middle_group__get()
-    pytest_func_tester__no_args_kwargs(func_link, _EXPECTED)
+class VictimMethCmp(VictimMeth):
+    MIDDLE_GROUP__CMP_METH = "meth_cmp"
 
 
-# ---------------------------------------------------------------------------------------------------------------------
+class VictimMethCmpMeth(VictimMethCmp):
+    def meth_cmp(self):
+        pass
+
+
+class VictimMethCmpMeth_TryBreak(VictimMethCmpMeth):
+    MIDDLE_GROUP__CMP_METH = "meth_cmp222"
+
+
+# =====================================================================================================================
 @pytest.mark.parametrize(
     argnames="obj, other, _EXPECTED",
     argvalues=[
@@ -119,12 +50,26 @@ def test__victim__name_get(obj, _EXPECTED):
 
         (Victim, Victim, True),
         (Victim, Victim(), True),
-        (Victim, Victim01, False),
-        (Victim, Victim01(), False),
-        (Victim, Victim02, False),
-        (Victim, Victim02(), False),
-        (Victim, Victim11, False),
-        (Victim, Victim11(), False),
+        (Victim, Victim10, False),
+        (Victim, Victim10(), False),
+
+        # VictimMeth
+        (Victim, VictimMeth, True),
+        (Victim(), VictimMeth, True),
+        (Victim(), VictimMeth(), True),
+        (Victim, VictimMeth(), True),
+
+        (Victim, VictimMethCmp, False),
+        (Victim(), VictimMeth, True),
+        (Victim(), VictimMeth(), True),
+        (Victim, VictimMeth(), True),
+
+        (VictimMeth, VictimMethCmpMeth, False),
+        (VictimMeth, VictimMethCmpMeth(), False),
+        (VictimMethCmp, VictimMethCmpMeth, False),
+        (VictimMethCmp, VictimMethCmpMeth(), False),
+
+        (VictimMethCmpMeth_TryBreak, VictimMethCmpMeth(), True),
     ]
 )
 def test__victim__check_equal(obj, other, _EXPECTED):
